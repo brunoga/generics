86521d5d999cfaf668ac6736f61f57554f3574c4
Bruno Albuquerque <bga@gmail.com>
2021-12-22 06:27:08 -0800

    API Cleanup.
    - Also make it restricted to Linux right now (only platform I tested).
      Should generally work on any unix-like platform anyway.

--- mmapslice/mmapslice.go
+++ mmapslice/mmapslice.go
@@ -1,3 +1,6 @@
+//go:build linux
+// +build linux
+
 package mmapslice
 
 import (
@@ -10,57 +13,77 @@ import (
 	"github.com/brunoga/generics/sliceconverter"
 )
 
-type MMapSlice[T1 any] []T1
+// MMapSlice is a slice of type T that is backed by a memory mapped file. You
+// can read and write to it normally but using append() will have unexpected
+// results if the slice must be resized (i.e. the data will not be the mmapped
+// one anymore which basically turns it into a normal slice
+type MMapSlice[T any] []T
 
-func (ms *MMapSlice[T]) Load(filename string) error {
+// Map maps the file represented by the given filename to a MMapSlice and
+// returns it. The file will be closed on return.
+func Map[T any](filename string) (MMapSlice[T], error) {
 	f, err := os.OpenFile(filename, os.O_RDWR, 0600)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	defer f.Close()
 
 	fi, err := f.Stat()
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	size := fi.Size()
 	if size == 0 {
-		return fmt.Errorf("tried to map an empty file")
+		return nil, fmt.Errorf("file %q is empty", filename)
 	}
-
 	if size < 0 {
-		return fmt.Errorf("file %q has negative size", filename)
+		return nil, fmt.Errorf("file %q has negative size", filename)
 	}
 	if size != int64(int(size)) {
-		return fmt.Errorf("file %q is too large", filename)
+		return nil, fmt.Errorf("file %q is too large", filename)
 	}
 
+	// Actually map the file to some of our address space.
 	data, err := syscall.Mmap(int(f.Fd()), 0, int(size), syscall.PROT_READ|
 		syscall.PROT_WRITE, syscall.MAP_SHARED)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	*ms = sliceconverter.Convert[byte, T](data)
+	// Create a slice that will interpret the data in the mapped memory as
+	// having the type we want. This does an in-place conversion so it works as
+	// expected
+	ms := sliceconverter.Convert[byte, T](data)
 
-	runtime.SetFinalizer(ms, (*MMapSlice[T]).Close)
+	// Make sure we unmap the memory when the slice goes out of scope.
+	runtime.SetFinalizer(ms, (*MMapSlice[T]).UnMap)
 
-	return nil
+	return ms, nil
 }
 
-func (ms MMapSlice[T]) Save(filename string) error {
-	return ioutil.WriteFile(filename, sliceconverter.Convert[T, byte](ms), 0600)
+// Save is a convenience function that saves the data associated with the given
+// slice to the file represented by the given filename. This file can later be
+// mapped (with the Map function above) to a MMapSlice.
+func Save[T any](slice []T, filename string) error {
+	return ioutil.WriteFile(filename, sliceconverter.Convert[T, byte](slice), 0600)
 }
 
-func (ms *MMapSlice[T]) Close() error {
+// UnMap unmaps the memory associated with the given MMapSlice. Any changes to
+// the given MMapSlice that were not persisted to the file will now be.
+func (ms *MMapSlice[T]) UnMap() error {
 	runtime.SetFinalizer(ms, nil)
 
+	// Unmap associated memory.
 	err := syscall.Munmap(sliceconverter.Convert[T, byte](*ms))
 	if err != nil {
 		return err
 	}
+
+	// Point out slice to an brand new empty slice. We need this as setting ms
+	// to nil wouild not quite work.
+	*ms = make(MMapSlice[T], 0)
 
 	return nil
 }
--- mmapslice/mmapslice_test.go
+++ mmapslice/mmapslice_test.go
@@ -6,26 +6,26 @@ import (
 )
 
 func TestMMapSlice(t *testing.T) {
-	mmapSlice := MMapSlice[int]([]int{1, 2, 3})
-	err := mmapSlice.Save("slice.mmap")
+	slice := []int{1, 2, 3}
+	err := Save(slice, "slice.mmap")
 	if err != nil {
 		t.Error(err)
 	}
 
 	defer func() { os.Remove("slice.mmap") }()
 
-	mmapSlice2 := MMapSlice[int]{}
-	err = mmapSlice2.Load("mmapslice_test")
+	mmapSlice, err := Map[int]("mmapslice_test")
 	if err != nil {
 		t.Error(err)
 	}
-
-	if len(mmapSlice) != len(mmapSlice2) {
-		t.Error("mmapslice and mmapSlice2 have different lengths")
+	if len(slice) != len(mmapSlice) {
+		t.Error("original slice and mmapSlice have different lengths")
 	}
-	for i, v := range mmapSlice {
-		if v != mmapSlice2[i] {
-			t.Errorf("mmapSlice[%d] = %d, mmapSlice2[%d] = %d", i, v, i, mmapSlice2[i])
+
+	for i, v := range slice {
+		if v != mmapSlice[i] {
+			t.Errorf("slice[%d] = %d, mmapSlice[%d] = %d", i, v, i,
+				mmapSlice[i])
 		}
 	}
 }
